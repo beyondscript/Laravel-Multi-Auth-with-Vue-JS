64e784c303036ac57086fd342e64cfee3e8aac00
Beyondscript <75921934+beyondscript@users.noreply.github.com>
2026-02-27 12:48:13 +0600

    24th commit

--- config/session.php
+++ config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug((env('APP_NAME', 'Laravel Multi Auth with Vue JS') ?: 'Laravel Multi Auth with Vue JS'), '_').'_session'
     ),
 
     /*
